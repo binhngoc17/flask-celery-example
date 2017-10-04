a2cc998a79c3eab2ccc159d944aa2385171f54dd
ngoc <ngoc.tran@honestbee.com>
2017-10-04 18:21:05 +0700

    Add change for celery beat

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask import Flask, request, render_template, session, flash, redirect, \
     url_for, jsonify
 from flask.ext.mail import Mail, Message
 from celery import Celery
+from celery.schedules import crontab
 
 
 app = Flask(__name__)
@@ -30,6 +31,24 @@ mail = Mail(app)
 celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
 celery.conf.update(app.config)
 
+
+@celery.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    # Calls test('hello') every 10 seconds.
+    sender.add_periodic_task(10.0, test.s('hello'), name='add every 10')
+
+    # Calls test('world') every 30 seconds
+    sender.add_periodic_task(30.0, test.s('world'), expires=10)
+
+    # Executes every Monday morning at 7:30 a.m.
+    sender.add_periodic_task(
+        crontab(hour=7, minute=30, day_of_week=1),
+        test.s('Happy Mondays!'),
+    )
+
+@celery.task
+def test(arg):
+    print(arg)
 
 @celery.task
 def send_async_email(msg):
